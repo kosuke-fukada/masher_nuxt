efaf30de71c879935aa329348df58ac4a58e7a7c
kosuke.fukada <j.lee.style.boogie.piano@gmail.com>
2022-03-05 22:28:04 +0900

    ignored git update.

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,5 +47,11 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: []
+  },
+
+  watchers: {
+    webpack: {
+      ignored: /(node_modules)|(.git)/
+    }
   }
 }
