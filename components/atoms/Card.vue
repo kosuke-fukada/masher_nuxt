01d0db860dcab5a99377c46c83f496937ddf27cf
kosuke.fukada <j.lee.style.boogie.piano@gmail.com>
2022-03-06 23:59:26 +0900

    fixed card transition.

--- components/atoms/Card.vue
+++ components/atoms/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mx-4 my-16 p-4 rounded-xl border cursor-pointer transition-all ease-in-out hover:-translate-y-1 hover:scale-105">
+  <div class="mx-4 my-16 p-4 rounded-xl border cursor-pointer transition-all ease-in-out hover:-translate-y-1 hover:shadow-lg">
     <slot />
   </div>
 </template>
