93f6470699435da1b733f2991381425a029e312a
kosuke.fukada <j.lee.style.boogie.piano@gmail.com>
2022-03-07 23:49:18 +0900

    added step component.

---
+++ components/atoms/Step.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex justify-center my-12">
+    <div class="grid w-3/4 grid-cols-3 bg-slate-200 rounded-xl overflow-hidden">
+      <div
+        v-for="(item, index) in list"
+        :key="index"
+        class="p-4"
+        :class="{
+          'bg-green-400': index <= active,
+          'rounded-r-xl': index === active
+        }"
+      >
+        <div class="flex justify-start text-white font-bold">
+          <span class="mr-1">{{ index + 1 }}.</span>
+          <span>{{ item.label }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Step',
+  props: {
+    list: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    active: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
