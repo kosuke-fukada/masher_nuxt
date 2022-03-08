d9b82ce3f35538a48cbc50254c707e5e591ddfaf
kosuke.fukada <j.lee.style.boogie.piano@gmail.com>
2022-03-08 23:33:38 +0900

    added dark mode toggle.

---
+++ components/organisms/DarkModeToggle.vue
@@ -0,0 +1,36 @@
+<template>
+  <Toggle
+    :checked="isDarkMode"
+    @change="darkModeToggle"
+  />
+</template>
+
+<script>
+import Toggle from '@/components/atoms/Toggle'
+
+export default {
+  name: 'DarkModeToggle',
+  components: {
+    Toggle
+  },
+  data() {
+    return {
+      isDarkMode: false
+    }
+  },
+  mounted() {
+    const isDarkMode = Boolean(parseInt(localStorage.getItem('isDarkMode')))
+    if (isDarkMode) {
+      this.isDarkMode = isDarkMode
+      this.$isDarkMode.isDarkMode = isDarkMode
+    }
+  },
+  methods: {
+    darkModeToggle() {
+      this.isDarkMode = !this.isDarkMode
+      this.$isDarkMode.isDarkMode = this.isDarkMode
+      localStorage.setItem('isDarkMode', Number(this.isDarkMode))
+    }
+  }
+}
+</script>
