7c4d516709cc88a0de432ab4798d059e61e05253
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-03-13 22:05:51 +0900

    added environment config.

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,15 +69,30 @@ export default {
     }
   },
 
+  env: {
+    FIRE_ENV: process.env.FIRE_ENV
+  },
+
   firebase: {
     config: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID,
-      measurementId: process.env.FIREBASE_MEASUREMENT_ID
+      development: {
+        apiKey: process.env.FIREBASE_API_KEY_DEV,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN_DEV,
+        projectId: process.env.FIREBASE_PROJECT_ID_DEV,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET_DEV,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID_DEV,
+        appId: process.env.FIREBASE_APP_ID_DEV,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID_DEV
+      },
+      production: {
+        apiKey: process.env.FIREBASE_API_KEY_PROD,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN_PROD,
+        projectId: process.env.FIREBASE_PROJECT_ID_PROD,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET_PROD,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID_PROD,
+        appId: process.env.FIREBASE_APP_ID_PROD,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID_PROD
+      }
     },
     services: {
       auth: true,
