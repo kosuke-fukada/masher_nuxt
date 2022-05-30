a760145cbf649535d94a1b0f3baee830d54cf146
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-05-30 20:24:33 +0900

    removed firebase.

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,8 +54,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    '@nuxtjs/firebase'
+    '@nuxtjs/axios'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -85,37 +84,8 @@ export default {
   },
 
   env: {
-    FIRE_ENV: process.env.FIRE_ENV,
     BACKEND_API_HOST: process.env.BACKEND_API_HOST,
     SESSION_SECRET: process.env.SESSION_SECRET
-  },
-
-  firebase: {
-    config: {
-      development: {
-        apiKey: process.env.FIREBASE_API_KEY_DEV,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN_DEV,
-        projectId: process.env.FIREBASE_PROJECT_ID_DEV,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET_DEV,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID_DEV,
-        appId: process.env.FIREBASE_APP_ID_DEV,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID_DEV
-      },
-      production: {
-        apiKey: process.env.FIREBASE_API_KEY_PROD,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN_PROD,
-        projectId: process.env.FIREBASE_PROJECT_ID_PROD,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET_PROD,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID_PROD,
-        appId: process.env.FIREBASE_APP_ID_PROD,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID_PROD
-      }
-    },
-    services: {
-      auth: true,
-      firestore: true,
-      analytics: true
-    }
   },
 
   server: {
