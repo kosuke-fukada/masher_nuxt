4737431370bc27abeb0822d42728bf554fd83549
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-05-31 23:34:54 +0900

    added loading component.

---
+++ components/atoms/Loading.vue
@@ -0,0 +1,55 @@
+<template>
+  <client-only>
+    <div
+      class="inset-1/2"
+      :class="position"
+    >
+      <Spinner
+        :name="name"
+        :color="color"
+        :width="width"
+        :height="height"
+      />
+    </div>
+  </client-only>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+  props: {
+    name: {
+      type: String,
+      default: 'circle-fade'
+    },
+    color: {
+      type: String,
+      default: '#FFFFFF'
+    },
+    fixed: {
+      type: Boolean,
+      default: false
+    },
+    width: {
+      type: String,
+      default: ''
+    },
+    height: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    position() {
+      return this.fixed ? 'fixed' : ''
+    }
+  }
+}
+</script>
+
+<style>
+  .sk-spinner>div {
+    width: 10px !important;
+    height: 10px !important;
+  }
+</style>
