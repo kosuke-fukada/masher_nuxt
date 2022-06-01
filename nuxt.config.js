8819e5827fe936feec5ad2a49e7f1d56da05a34a
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-06-01 21:52:52 +0900

    added toast.

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,8 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/toast'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
