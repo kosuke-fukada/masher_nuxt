5747a0d92977959a0c98e143938e90a3471f749d
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-06-13 22:59:40 +0900

    added try/catch to callback page.

--- pages/signin/callback.vue
+++ pages/signin/callback.vue
@@ -6,6 +6,7 @@
 
 <script>
 import Loading from '~/components/atoms/Loading'
+import ForbiddenError from '~/errors/ForbiddenError'
 export default {
   name: 'Callback',
   components: {
@@ -23,17 +24,35 @@ export default {
     const params = {
       params: query
     }
-    const response = await this.$axios.$get('/api/signin/twitter/callback/', params)
-    if (Object.prototype.hasOwnProperty.call(response, 'message')) {
-      this.$router.replace('/')
+    try {
+      const response = await this.$axios.$get('/api/signin/twitter/callback/', params)
+      if (Object.prototype.hasOwnProperty.call(response, 'message')) {
+        this.$router.replace('/')
+      }
+      const userInfo = await this.$axios.$get('/api/user/')
+      if (userInfo.user_name) {
+        await this.$store.dispatch('user/setUserInfo', userInfo)
+      } else {
+        await this.$store.dispatch('user/clearUser')
+      }
+      this.$router.replace('/main')
+    } catch (e) {
+      if (e instanceof ForbiddenError) {
+        this.$router.replace({
+          path: '/',
+          query: {
+            cancelled: 'true'
+          }
+        })
+      } else {
+        this.$router.replace({
+          path: '/',
+          query: {
+            error: 'true'
+          }
+        })
+      }
     }
-    const userInfo = await this.$axios.$get('/api/user/')
-    if (userInfo.user_name) {
-      await this.$store.dispatch('user/setUserInfo', userInfo)
-    } else {
-      await this.$store.dispatch('user/clearUser')
-    }
-    this.$router.replace('/main')
   }
 }
 </script>
